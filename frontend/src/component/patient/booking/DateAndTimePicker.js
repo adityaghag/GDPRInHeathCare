70395679ae20448c99b9021d14ab89d622734ccb
Mohamad Bouchi <mohamadbouchi@Mohamads-MacBook-Pro.local>
2020-01-11 13:13:09 +0100

    date picker format

--- frontend/src/component/patient/booking/DateAndTimePicker.js
+++ frontend/src/component/patient/booking/DateAndTimePicker.js
@@ -23,7 +23,7 @@ export default function DateAndTimePicker(props) {
                 <KeyboardDatePicker
                     disableToolbar
                     variant="inline"
-                    format="MM/dd/yyyy"
+                    format="dd/MM/yyyy"
                     margin="normal"
                     id="date-picker-inline"
                     label="Select Date"
