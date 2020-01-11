bcdddac329105650687bc08e569854d014b2a61d
Mohamad Bouchi <mohamadbouchi@Mohamads-MacBook-Pro.local>
2020-01-11 15:35:43 +0100

    test results component

--- frontend/src/component/lab/BookingAppointment.js
+++ frontend/src/component/lab/BookingAppointment.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import LabDrawer from './LabDrawer';
-
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import 'date-fns';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -19,15 +28,79 @@ const useStyles = makeStyles(theme => ({
 
 export default function BookingAppointment() {
     const classes = useStyles();
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+    const handleDateChange = date => {
+        setSelectedDate(date);
+    };
 
     return (
         <div className={classes.root}>
             <LabDrawer />
             <main className={classes.content}>
                 <div className={classes.toolbar} />
-                <Typography paragraph>
-                    Booking Appointment
-                </Typography>
+                <Container component="main" maxWidth="md">
+                    <CssBaseline />
+                    <div className={classes.paper}>
+                        <Typography component="h1" variant="h5">Book Appointment</Typography>
+                        <br />
+                        <form className={classes.form} noValidate>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="patientID"
+                                        label="patientID"
+                                        name="patientID"
+                                        autoComplete="patientID"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <NativeSelect
+                                        fullWidth
+                                        name="category"
+                                        inputProps={{ 'aria-label': 'category' }}
+                                    >
+                                        <option value="" disabled>
+                                            Categories
+                                        </option>
+                                        <option value={10}>Ten</option>
+                                        <option value={20}>Twenty</option>
+                                        <option value={30}>Thirty</option>
+                                    </NativeSelect>
+                                    <FormHelperText>Categories</FormHelperText>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                        <KeyboardDatePicker
+                                            fullWidth
+                                            disableToolbar
+                                            variant="inline"
+                                            format="dd/MM/yyyy"
+                                            margin="normal"
+                                            id="date-picker-inline"
+                                            label="Select Date"
+                                            value={selectedDate}
+                                            onChange={handleDateChange}
+                                            KeyboardButtonProps={{
+                                                'aria-label': 'change date',
+                                            }}
+                                        />
+                                    </MuiPickersUtilsProvider>
+                                </Grid>
+                            </Grid>
+                            <br></br>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="outlined"
+                                color="primary"
+                            >save</Button>
+                        </form>
+                    </div>
+                </Container>
             </main>
         </div>
     );
--- frontend/src/component/lab/Testresults.js
+++ frontend/src/component/lab/Testresults.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import LabDrawer from './LabDrawer';
-
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 
 const useStyles = makeStyles(theme => ({
   root: {
+    width: '100%'
+  },
+  mainRoot: {
     display: 'flex',
   },
   toolbar: theme.mixins.toolbar,
@@ -13,21 +22,113 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     backgroundColor: theme.palette.primary.default,
     padding: theme.spacing(3),
+  },
+  container: {
+    maxHeight: 440,
   }
 }));
 
 
+
+const columns = [
+  { id: 'patientId', label: 'Patient\u00a0Id', minWidth: 100 },
+  {
+    id: 'testType',
+    label: 'Test\u00a0Type',
+    minWidth: 170,
+    align: 'right',
+    format: value => value.toLocaleString(),
+  },
+  {
+    id: 'date',
+    label: 'Date\u00a0',
+    minWidth: 170,
+    align: 'right',
+    format: value => value.toLocaleString(),
+  }
+];
+
+function createData(patientId, testType, date) {
+
+  return { patientId, testType, date };
+}
+
+const rows = [
+  createData('11111', 'Bold', '11/12/2019'),
+  createData('22222', 'Bold', '11/12/2019'),
+  createData('33333', 'Bold', '11/12/2019'),
+  createData('44444', 'Bold', '11/12/2019'),
+  createData('55555', 'Bold', '11/12/2019'),
+  createData('66666', 'Bold', '11/12/2019'),
+  createData('77777', 'Bold', '11/12/2019'),
+  createData('88888', 'Bold', '11/12/2019'),
+  createData('99999', 'Bold', '11/12/2019'),
+];
+
 export default function Testresults() {
   const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
 
   return (
-    <div className={classes.root}>
+    <div className={classes.mainRoot}>
       <LabDrawer />
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Test Results
-        </Typography>
+        <Paper className={classes.root}>
+          <TableContainer className={classes.container}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map(column => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.patientId}>
+                      {columns.map(column => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === 'number' ? column.format(value) : value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        </Paper>
       </main>
     </div>
   );
