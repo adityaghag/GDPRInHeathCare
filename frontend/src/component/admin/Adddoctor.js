93259da43f8b282775fd31df939f045830b6549d
Mohamad Bouchi <mohamadbouchi@Mohamads-MacBook-Pro.local>
2020-01-11 13:47:21 +0100

    add doctor form

--- frontend/src/component/admin/Adddoctor.js
+++ frontend/src/component/admin/Adddoctor.js
@@ -1,6 +1,21 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import AdminDrawer from './AdminDrawer';
+import React, { useState } from 'react';
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormLabel from '@material-ui/core/FormLabel';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
 
 const drawerWidth = 240;
 
@@ -30,14 +45,160 @@ const useStyles = makeStyles(theme => ({
 
 export default function Adddoctor() {
   const classes = useStyles();
+  const [selectedDate, handleDateChange] = useState(new Date());
+  const [value, setValue] = React.useState('female');
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
 
   return (
     <div className={classes.root}>
       <AdminDrawer />
       <main className={classes.content}>
         <div className={classes.toolbar} />
-
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Registration
+        </Typography>
+            <form className={classes.form} noValidate>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="fname"
+                    name="firstName"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="lname"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <DatePicker
+                      disableFuture
+                      openTo="year"
+                      required
+                      format="dd/MM/yyyy"
+                      label="Date of birth"
+                      views={["year", "month", "date"]}
+                      value={selectedDate}
+                      onChange={handleDateChange}
+                    />
+                  </MuiPickersUtilsProvider>
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    fullWidth
+                    id="insurancenum"
+                    label="Insurance Number"
+                    name="insurancenum"
+                    autoComplete="insurancenum"
+                  />
+                </Grid>
+                <Grid item xs={1}>
+                  <FormLabel component="legend">Gender</FormLabel>
+                  <RadioGroup aria-label="gender" name="gender2" value={value} onChange={handleChange} required>
+                    <FormControlLabel
+                      value="female"
+                      control={<Radio color="primary" />}
+                      label="Female"
+                    />
+                    <FormControlLabel
+                      value="male"
+                      control={<Radio color="primary" />}
+                      label="Male"
+                    />
+                    <FormControlLabel
+                      value="other"
+                      control={<Radio color="primary" />}
+                      label="Other"
+                    />
+                  </RadioGroup>
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    fullWidth
+                    id="phonenum"
+                    label="Phone Number"
+                    name="phonenum"
+                    autoComplete="phonenum"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    fullWidth
+                    multiline
+                    rows="4"
+                    id="address"
+                    label="Address"
+                    name="address"
+                    autoComplete="address"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                  />
+                </Grid>
+              </Grid>
+              <br></br>
+              <Button
+                type="submit"
+                fullWidth
+                variant="outlined"
+                className={classes.submit}
+              >
+                Register
+          </Button>
+            </form>
+          </div>
+        </Container>
       </main>
     </div>
   );
 }
+
+
+
+
+
