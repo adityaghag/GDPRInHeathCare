0e47520bbd5c758f684e96a8ba4bfde5237b7268
Mohamad Bouchi <mohamadbouchi@Mohamads-MacBook-Pro.local>
2020-01-16 18:41:14 +0100

    refactoring

--- frontend/src/component/patient/booking/Booking.js
+++ frontend/src/component/patient/booking/Booking.js
@@ -1,35 +1,16 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import { Link } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import { MenuList, MenuItem } from '@material-ui/core';
 import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Patient from '../Patient'
 
-const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
-  },
-  appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
   },
   toolbar: theme.mixins.toolbar,
   content: {
@@ -53,43 +34,7 @@ export default function Booking() {
 
   return (
     < div className={classes.root} >
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            Appointment Booking
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
-        <MenuList>
-          <MenuItem component={Link} to="/patient">Home</MenuItem>
-        </MenuList>
-        <Divider />
-        <MenuList>
-          <MenuItem component={Link} to="/reportupload">Upload Reports</MenuItem>
-        </MenuList>
-        <Divider />
-        <MenuList>
-          <MenuItem component={Link} to="/booking">Booking Appointment</MenuItem>
-        </MenuList>
-        <Divider />
-        <MenuList>
-          <MenuItem component={Link} to="/reportview">View Reports</MenuItem>
-        </MenuList>
-        <Divider />
-        <MenuList>
-          <MenuItem component={Link} to="/patient">Logout</MenuItem>
-        </MenuList>
-      </Drawer>
+      <Patient />
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <Grid
