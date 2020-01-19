d25851b4caf4159d8c927d0497ee91a4c3efe54e
Mohamad Bouchi <mohamadbouchi@Mohamads-MacBook-Pro.local>
2020-01-19 19:04:02 +0100

    cancel appointment

--- frontend/src/component/doctor/Doctor.js
+++ frontend/src/component/doctor/Doctor.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import DoctorDrawer from './DoctorDrawer';
+import Grid from '@material-ui/core/Grid';
+import { Context } from '../../store/Store';
+import Loading from '../Loading';
+import Cards from './Cards';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,18 +18,59 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+function createCard(patientsAppos) {
+
+
+  let card = []
+
+  patientsAppos.map((item) => {
+    return card.push(<Grid item key={item._id} xs={3}><Cards day={item.day} /></Grid>)
+  });
+
+  if (card.length === 0)
+    return <div style={{ width: '100%', textAlign: 'center' }}><h2>You have no upcoming Appointments</h2></div>
+  return card
+}
 
 export default function Doctor() {
+  const [state, dispatch] = useContext(Context);
   const classes = useStyles();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const doctorId = {
+        doctorId: localStorage.getItem('userId')
+      }
+      const res = await fetch("http://localhost:3001/appointments/getAllAppointmentsOfDoctors", {
+        method: 'post',
+        body: JSON.stringify(doctorId),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+      res.json().then(res => {
+        console.log(res)
+        dispatch({ type: 'SET_PATIENT_APPOS', payload: res.appointments });
+        dispatch({ type: 'SET_LOADING', payload: false });
+      })
+    }
+    fetchData();
+    return () => {
+      dispatch({ type: 'SET_LOADING', payload: true });
+    };
+  }, [dispatch]);
   return (
     <div className={classes.root}>
       <DoctorDrawer />
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Welcome Doctor !
-        </Typography>
+        {
+          state.loading ? <Loading /> :
+            <Grid container spacing={2}>
+              {createCard(state.patientAppos)}
+            </Grid>
+        }
       </main>
     </div>
   );
--- frontend/src/component/patient/AppointmentsCard.js
+++ frontend/src/component/patient/AppointmentsCard.js
@@ -1,23 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-
+import { Context } from '../../store/Store';
 
 const useStyles = makeStyles(theme => ({
-
-    card: {
-        maxWidth: 300,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
     title: {
         fontSize: 14,
     },
@@ -28,17 +18,36 @@ const useStyles = makeStyles(theme => ({
 
 
 export default function AppointmentsCard(props) {
+    const [, dispatch] = useContext(Context);
     let day = props.day;
     let cat = props.cat;
     let firstName = props.firstName;
     let lastName = props.lastName;
 
-
+    const fetchData = async () => {
+        const appointmentId = {
+            appointmentId: props.id
+        }
+        const res = await fetch("http://localhost:3001/appointments/deleteAppointment", {
+            method: 'post',
+            body: JSON.stringify(appointmentId),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        res.json().then(res => {
+            dispatch({ type: 'SET_RELOAD' });
+        })
+    }
+    const handleClick = () => {
+        fetchData()
+    }
 
     const classes = useStyles();
 
     return (
-        <Card className={classes.card}>
+        <Card>
             <CardContent>
                 <Typography variant="h6" gutterBottom>
                     You have an Appointment on: {day}
@@ -51,7 +60,7 @@ export default function AppointmentsCard(props) {
                 </Typography>
             </CardContent>
             <CardActions>
-
+                <Button variant="outlined" color="secondary" onClick={handleClick}>Cancel Appointment</Button>
             </CardActions>
         </Card>
     );
